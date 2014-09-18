81ce8469fc42bdc787654ea6b6c239cd56eebdf7
Gregory LaFlash <gregorylaflash@gmail.com>
2014-09-17 22:26:22 -0500

    Added models for Posts and Users and added API to list, create, and delete

--- app/models/Post.js
+++ app/models/Post.js
@@ -2,6 +2,8 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Post', {
-	text : String,
-	done : Boolean
+	content: { type: String, required: true },
+	author: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+	date_created: { type: Date, default: Date.now(), required: true},
+	image: String
 });
